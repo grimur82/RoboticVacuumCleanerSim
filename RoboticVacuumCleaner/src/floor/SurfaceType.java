6438d9c50d5649c21df07d9a12ed3c861b74498d
mrtuanma <tuan.ma@me.com>
2016-10-18 08:27:29 -0700

    Updated SurfaceType
    Added enum code for surface type with a return of power used based on
    surface type.

--- RoboticVacuumCleaner/src/floor/SurfaceType.java
+++ RoboticVacuumCleaner/src/floor/SurfaceType.java
@@ -4,10 +4,20 @@ package floor;
  * Maps surface type to the amount of power used.
  */
 public enum SurfaceType {
-
-    NULL, // 0
-    BARE, // 1
-    LOWPILE, // 2
-    HIGHPILE // 3
+    
+    NONE(0),
+    BARE(1),
+    LOWPILE(2),
+    HIGHPILE(3);
+    
+    private int powerUsed;
+    
+    Type(int powerUsed){
+        this.powerUsed = powerUsed;
+    }
+    
+    public int getSurfaceType() {
+        return powerUsed;
+    }
 
 }
