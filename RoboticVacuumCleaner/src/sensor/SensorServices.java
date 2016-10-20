6ae682c99984fe018249aecabc7a7c03f8add543
Amalyn Tago <amalyn.a.tago@gmail.com>
2016-10-20 03:04:34 -0500

    Test

--- RoboticVacuumCleaner/src/sensor/SensorServices.java
+++ RoboticVacuumCleaner/src/sensor/SensorServices.java
@@ -14,60 +14,61 @@ import java.io.IOException;
 
 public class SensorServices {
 
-    private static SensorServices sensorServices;
-    private static Cell[][] floorPlan;
-    private static Coordinate startPosition;
-    private static ArrayList<Coordinate> doorList;
-   
+	private static SensorServices sensorServices;
+	private static Cell[][] floorPlan;
+	private static Coordinate startPosition;
+	private static ArrayList<Coordinate> doorList;
 
-    private SensorServices() throws ParserConfigurationException, SAXException, IOException {
-        Debugger.log("Starting sensor simulator");
-        FloorPlanLoader floorPlanLoader = new FloorPlanLoader();
-        floorPlan = floorPlanLoader.getFloorPlan();
-        startPosition = floorPlanLoader.getStartPosition();
-        doorList = floorPlanLoader.getDoors();
-    }
+	private SensorServices() throws ParserConfigurationException, SAXException,
+			IOException {
+		Debugger.log("Starting sensor simulator");
+		FloorPlanLoader floorPlanLoader = new FloorPlanLoader();
+		floorPlan = floorPlanLoader.getFloorPlan();
+		startPosition = floorPlanLoader.getStartPosition();
+		doorList = floorPlanLoader.getDoors();
+	}
 
-    public static SensorServices getInstance() throws ParserConfigurationException, SAXException, IOException {
-        if (sensorServices == null)
-            sensorServices = new SensorServices();
+	public static SensorServices getInstance()
+			throws ParserConfigurationException, SAXException, IOException {
+		if (sensorServices == null)
+			sensorServices = new SensorServices();
 
-        return sensorServices;
-    }
+		return sensorServices;
+	}
 
-    public Cell getCell(int x, int y) {
-        return floorPlan[x][y];
-    }
+	public Cell getCell(int x, int y) {
+		return floorPlan[x][y];
+	}
 
-    public Coordinate getStartPosition() {
-        return startPosition;
-    }
-    
-    public boolean senseObstacleTop(Cell cell){
-    	if(cell.getObstacles().contains(Obstacle.TOP))
-    		return true;
+	public Coordinate getStartPosition() {
+		return startPosition;
+	}
+
+	public boolean senseObstacleTop(Cell cell) {
+		if (cell.getObstacles().contains(Obstacle.TOP))
+			return true;
 		return false;
-    }
-    
-    public boolean senseObstacleBottom(Cell cell){
-    	if(cell.getObstacles().contains(Obstacle.BOTTOM))
-    		return true;
+	}
+
+	public boolean senseObstacleBottom(Cell cell) {
+		if (cell.getObstacles().contains(Obstacle.BOTTOM))
+			return true;
 		return false;
-    }
-    
-    public boolean senseObstacleLeft(Cell cell){
-    	if(cell.getObstacles().contains(Obstacle.LEFT))
-    		return true;
+	}
+
+	public boolean senseObstacleLeft(Cell cell) {
+		if (cell.getObstacles().contains(Obstacle.LEFT))
+			return true;
 		return false;
-    }
-    
-    public boolean senseObstacleRight(Cell cell){
-    	if(cell.getObstacles().contains(Obstacle.RIGHT))
-    		return true;
+	}
+
+	public boolean senseObstacleRight(Cell cell) {
+		if (cell.getObstacles().contains(Obstacle.RIGHT))
+			return true;
 		return false;
-    }
-    
-    public boolean senseDoor(){
-    	
-    }
+	}
+
+	public boolean senseDoor() {
+
+	}
 }
