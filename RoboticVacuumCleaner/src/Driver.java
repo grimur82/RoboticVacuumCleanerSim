2b435621141316eda2c2e740a1ea287163939e5f
tifa <tifa@tifa.io>
2016-10-19 20:32:14 -0500

    Copied from my branch "moving"

--- RoboticVacuumCleaner/src/Driver.java
+++ RoboticVacuumCleaner/src/Driver.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 
 import navigation.ControlSystemService;
-import navigation.Navigation;
 
 import org.xml.sax.SAXException;
 
@@ -13,13 +12,6 @@ public class Driver {
 
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 		SensorServices sL = SensorServices.getInstance();
-		ControlSystemService cS = ControlSystemService.getInstance();
-		sL.loadFloorPlan();
-		System.out.println("Start x: " + sL.getStartPosition().getX());
-		cS.setNavigation(sL.getStartPosition());
-		sL.getLeftPosition();
-		sL.cleanDirt(
-		(int)sL.getLeftPosition().getX(), (int)sL.getLeftPosition().getY());
-		sL.cleanDirt((int)sL.getStartPosition().getX(), (int) sL.getStartPosition().getY());
+		ControlSystemService.getInstance(sL);
 	}
 }
