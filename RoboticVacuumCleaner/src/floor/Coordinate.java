5c2308700c549874cba6345d7d5fd39a801fdc67
grimur82 <grimur82@gmail.com>
2016-11-03 20:12:07 -0500

    Added obstacles when they sweeper goes back to base.

--- RoboticVacuumCleaner/src/floor/Coordinate.java
+++ RoboticVacuumCleaner/src/floor/Coordinate.java
@@ -37,63 +37,131 @@ public class Coordinate {
     	}
     	return parents;
     }
+    public boolean checkObstacle(double x, double y, String check){
+    	if(check.equals("bottom")){
+    		Cell cell = SensorServices.getInstance().getCell((int)x, (int)y);
+    		return SensorServices.getInstance().senseObstacleBottom(cell);
+    	}
+    	if(check.equals("left")){
+    		Cell cell = SensorServices.getInstance().getCell((int)x, (int)y);
+    		return SensorServices.getInstance().senseObstacleLeft(cell);
+    	}
+    	if(check.equals("right")){
+    		Cell cell = SensorServices.getInstance().getCell((int)x, (int)y);
+    		return SensorServices.getInstance().senseObstacleRight(cell);
+    	}
+    	if(check.equals("top")){
+    		Cell cell = SensorServices.getInstance().getCell((int)x, (int)y);
+    		return SensorServices.getInstance().senseObstacleTop(cell);
+    	}
+		return false;
+    	
+    }
     public void setParents(){
     	if(parents == null){
     		parents = new ArrayList<Coordinate>();
     	}
     	// Corners:
-    	if(x == 0 && y ==0){		
-    		parents.add(new Coordinate(x,y+1));
-    		parents.add(new Coordinate(x+1,y));
+    	if(x == 0 && y ==0){
+    		if(checkObstacle(x,y+1,"top") == false){
+    			parents.add(new Coordinate(x,y+1));
+    		}
+    		if(checkObstacle(x,y-1,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
     		return;
     	}
     	if(x == SensorServices.getInstance().getFloorPlan().length-1 && y == 0){
-    		parents.add(new Coordinate(x,y+1));
-    		parents.add(new Coordinate(x-1,y));
+    		if(checkObstacle(x,y+1,"top") == false){
+    			parents.add(new Coordinate(x,y+1));
+    		}
+    		if(checkObstacle(x-1,y,"left") == false){
+    			parents.add(new Coordinate(x-1,y));
+    		}
     		return;
     	}
     	if(x == 0 && y ==SensorServices.getInstance().getFloorPlan().length-1){
-    		parents.add(new Coordinate(x+1,y));
-    		parents.add(new Coordinate(x,y-1));
+    		if(checkObstacle(x+1,y,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
+    		if(checkObstacle(x,y-1,"bottom") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}	
     		return;
     	}
     	if(x == SensorServices.getInstance().getFloorPlan().length-1 && y ==SensorServices.getInstance().getFloorPlan().length-1){
-    		parents.add(new Coordinate(x-1,y));
-			parents.add(new Coordinate(x,y-1));
+    		if(checkObstacle(x-1,y,"left") == false){
+    			parents.add(new Coordinate(x-1,y));
+    		}
+    		if(checkObstacle(x,y-1,"bottom") == false){
+    			parents.add(new Coordinate(x,y-1));
+    		}
     	  	return;
     	}
     	// Sides:
     	if(x >0 && x < SensorServices.getInstance().getFloorPlan().length-1 && y ==0){
-    		parents.add(new Coordinate(x,y+1));
-    		parents.add(new Coordinate(x-1,y));
-    		parents.add(new Coordinate(x+1,y));
+    		if(checkObstacle(x+1,y,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
+    		if(checkObstacle(x-1,y,"left") == false){
+    			parents.add(new Coordinate(x-1,y));
+    		}
+    		if(checkObstacle(x+1,y,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
     		return;
     	}
     	if(x ==0 && x > 0 && y < SensorServices.getInstance().getFloorPlan().length-1){
-    		parents.add(new Coordinate(x,y+1));
-    		parents.add(new Coordinate(x,y-1));
-    		parents.add(new Coordinate(x+1,y));
+    		if(checkObstacle(x,y+1,"top") == false){
+    			parents.add(new Coordinate(x,y+1));
+    		}
+    		if(checkObstacle(x,y-1,"bottom") == false){
+    			parents.add(new Coordinate(x,y-1));
+    		}
+    		if(checkObstacle(x+1,y,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
     		return;
     	}
     	if(x >0 && x < SensorServices.getInstance().getFloorPlan().length-1 && y == SensorServices.getInstance().getFloorPlan().length-1){
-    		parents.add(new Coordinate(x+1,y));
-    		parents.add(new Coordinate(x,y-1));
-    		parents.add(new Coordinate(x-1,y));
+    		if(checkObstacle(x+1,y,"right") == false){
+    			parents.add(new Coordinate(x+1,y));
+    		}
+    		if(checkObstacle(x,y-1,"bottom") == false){
+    			parents.add(new Coordinate(x,y-1));
+    		}
+    		if(checkObstacle(x-1,y,"left") == false){
+    			parents.add(new Coordinate(x-1,y));
+    		}
     		return;
     	}
     	if(x == SensorServices.getInstance().getFloorPlan().length-1 && y > 0 && y < SensorServices.getInstance().getFloorPlan().length-1){
-    		parents.add(new Coordinate(x-1,y));
-    		parents.add(new Coordinate(x,y+1));
-    		parents.add(new Coordinate(x,y-1));
+    		if(checkObstacle(x-1,y,"left") == false){
+    			parents.add(new Coordinate(x-1,y));
+    		}
+    		if(checkObstacle(x,y+1,"top") == false){
+    			parents.add(new Coordinate(x,y+1));
+    		}
+    		if(checkObstacle(x,y-1,"bottom") == false){
+    			parents.add(new Coordinate(x,y-1));
+    		}
     		return;
     	}
     	// Else:
     	else{
     		if(x > 0 && x < SensorServices.getInstance().getFloorPlan().length-1 && y >0 && y < SensorServices.getInstance().getFloorPlan().length-1){
-    			parents.add(new Coordinate(x,y-1));
-        		parents.add(new Coordinate(x,y+1));
-        		parents.add(new Coordinate(x+1,y));
-        		parents.add(new Coordinate(x-1,y));
+    			if(checkObstacle(x-1,y,"left") == false){
+        			parents.add(new Coordinate(x-1,y));
+        		}
+        		if(checkObstacle(x,y+1,"top") == false){
+        			parents.add(new Coordinate(x,y+1));
+        		}
+        		if(checkObstacle(x,y-1,"bottom") == false){
+        			parents.add(new Coordinate(x,y-1));
+        		}
+        		if(checkObstacle(x+1,y,"right") == false){
+        			parents.add(new Coordinate(x+1,y));
+        		}
         		return;	
     		}		 
     	}
