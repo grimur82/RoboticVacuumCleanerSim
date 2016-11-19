d24996a2735855af2b2ecfa7a7cf275dc64cf0ec
mrtuanma <tuan.ma@me.com>
2016-11-18 17:44:13 -0800

    Add consumer log activity.

--- RoboticVacuumCleaner/src/util/LogActivity.java
+++ RoboticVacuumCleaner/src/util/LogActivity.java
@@ -35,9 +35,34 @@ class LogActivity {
 
 		} catch (IOException ex) {
 			System.out.println("Error writing to file '" + fileName + "'");
-			// Or we could just do this:
-			// ex.printStackTrace();
+			
 		}
+        
+        // The name of the file to open.
+		String fileName = "consumerlog.txt";
+
+		try {
+			// Assume default encoding.
+			FileWriter fileWriter = new FileWriter(fileName, true);
+
+			// Always wrap FileWriter in BufferedWriter.
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+			// Note that write() does not automatically
+			// append a newline character.
+			
+			bufferedWriter.write(str+"\r\n");
+
+			// Always close files.
+			bufferedWriter.close();
+
+			fileWriter.close();
+
+		} catch (IOException ex) {
+			System.out.println("Error writing to file '" + fileName + "'");
+
+		}
+        
 
 	}
 
